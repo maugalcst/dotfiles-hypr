41ad59aad7d01e185c28b62cf3592f6cc9d275a7
maugalcst <maugal.cst@gmail.com>
2026-07-18 13:52:04 -0600

    feat: wallpicker rofi theme + pywal template

--- scripts/wallpicker.sh
+++ scripts/wallpicker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 WALLDIR="$HOME/Pictures/wallpapers"
+THEME="$HOME/.cache/wal/wallpicker.rasi"
 
 PICK=$(find "$WALLDIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | sort | while read -r img; do
     echo -en "$(basename "$img")\0icon\x1f$img\n"
-done | rofi -dmenu -i -p "Wallpaper" -show-icons -theme-str 'element-icon { size: 6em; }')
+done | rofi -dmenu -i -p "  Wallpaper" -show-icons -theme "$THEME")
 
 [[ -z "$PICK" ]] && exit 0
 
